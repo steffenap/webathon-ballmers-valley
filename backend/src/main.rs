efbccef04dd31fbfa3d85fe68bc41f57b7b88ef0
Jakob Skaar <jakobrs100@gmail.com>
2026-04-11 18:36:17 +0200

    more work

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,11 +2,8 @@ use std::{collections::HashMap, sync::Arc};
 
 use axum::{
     Json, Router,
-    body::Body,
     extract::{Path, State},
-    http::{StatusCode, header},
-    response::Html,
-    routing::{get, post},
+    routing::{delete, get, post},
 };
 use serde::{Deserialize, Serialize};
 use sqlx::SqlitePool;
@@ -40,22 +37,31 @@ async fn group_members(
     }
 }
 
-async fn tasks(Path(group_id): Path<u32>, State(state): State<Arc<AppState>>) -> Json<HashMap<u32, String>> {
+async fn tasks(
+    Path(group_id): Path<u32>,
+    State(state): State<Arc<AppState>>,
+) -> Json<HashMap<u32, String>> {
     let mut conn = state.db.acquire().await.unwrap();
 
-    let rec = sqlx::query!("select id, title from tasks where gr = ?1", group_id).fetch_all(&mut *conn).await.unwrap();
+    let rec = sqlx::query!("select id, title from tasks where gr = ?1", group_id)
+        .fetch_all(&mut *conn)
+        .await
+        .unwrap();
 
     Json(rec.into_iter().map(|a| (a.id as u32, a.title)).collect())
 }
 async fn task_title(Path(task_id): Path<u32>, State(state): State<Arc<AppState>>) -> Json<String> {
     let mut conn = state.db.acquire().await.unwrap();
 
-    let rec = sqlx::query!("select title from tasks where id = ?1", task_id).fetch_one(&mut *conn).await.unwrap();
+    let rec = sqlx::query!("select title from tasks where id = ?1", task_id)
+        .fetch_one(&mut *conn)
+        .await
+        .unwrap();
 
     Json(rec.title)
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Copy, Clone)]
 enum TaskType {
     Assignment,
     Social,
@@ -63,16 +69,129 @@ enum TaskType {
     Reading,
 }
 
+impl TaskType {
+    fn to_str(self) -> &'static str {
+        match self {
+            TaskType::Assignment => "Assignment",
+            TaskType::Social => "Social",
+            TaskType::StudyHall => "StudyHall",
+            TaskType::Reading => "Reading",
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize)]
 struct TaskCreationData {
     title: String,
     r#type: Option<TaskType>,
-    due: Option<String>,
+    due: Option<u64>,
     reward: i32,
+    group: Option<u32>,
 }
-async fn create_task(Path(group_id): Path<u32>, Json(data): Json<TaskCreationData>) -> Json<()> {
+async fn create_task(
+    State(state): State<Arc<AppState>>,
+    Json(data): Json<TaskCreationData>,
+) -> Json<()> {
+    let mut conn = state.db.acquire().await.unwrap();
+
+    let TaskCreationData {
+        title,
+        r#type,
+        due,
+        reward,
+        group,
+    } = data;
+
+    let r#type = r#type.map(|x| x.to_str());
+    let due = due.map(|x| x as i64);
+
+    sqlx::query!(
+        "insert into tasks(title, type, due, reward, gr) values (?1, ?2, ?3, ?4, ?5)",
+        title,
+        r#type,
+        due,
+        reward,
+        group,
+    )
+    .execute(&mut *conn)
+    .await
+    .unwrap();
+
     Json(())
 }
+
+async fn delete_task(Path(task_id): Path<u32>, State(state): State<Arc<AppState>>) -> Json<()> {
+    let mut conn = state.db.acquire().await.unwrap();
+
+    sqlx::query!("delete from tasks where id = ?1", task_id)
+        .execute(&mut *conn)
+        .await
+        .unwrap();
+
+    Json(())
+}
+
+#[derive(Deserialize)]
+struct GroupCreationData {
+    title: String,
+}
+async fn create_group(
+    State(state): State<Arc<AppState>>,
+    Json(group_info): Json<GroupCreationData>,
+) -> Json<()> {
+    let mut conn = state.db.acquire().await.unwrap();
+
+    sqlx::query!("insert into groups(name) values (?1)", group_info.title)
+        .execute(&mut *conn)
+        .await
+        .unwrap();
+
+    Json(())
+}
+
+async fn delete_group(Path(group_id): Path<u32>, State(state): State<Arc<AppState>>) -> Json<()> {
+    let mut conn = state.db.acquire().await.unwrap();
+
+    sqlx::query!("delete from groups where id = ?1", group_id)
+        .execute(&mut *conn)
+        .await
+        .unwrap();
+
+    // Json(())
+    Json(())
+}
+
+#[derive(Deserialize)]
+struct GroupInviteData {
+    user_id: u32,
+}
+
+async fn invite_to_group(
+    Path(group_id): Path<u32>,
+    State(state): State<Arc<AppState>>,
+    Json(invite_info): Json<GroupInviteData>,
+) -> Json<()> {
+    let mut conn = state.db.acquire().await.unwrap();
+
+    sqlx::query!(
+        "insert into user_group(user, gr) values (?1, ?2)",
+        group_id,
+        invite_info.user_id
+    )
+    .execute(&mut *conn)
+    .await
+    .unwrap();
+
+    Json(())
+}
+
+#[derive(Deserialize)]
+struct LoginData {
+    username: String,
+    password: String,
+}
+
+async fn login() {}
 
 struct AppState {
     db: SqlitePool,
@@ -91,22 +210,38 @@ async fn main() -> anyhow::Result<()> {
         ("/assignments", "assignments.html"),
         ("/group", "group.html"),
         ("/utils.js", "utils.js"),
+        ("/images/grinder.png", "images/grinder.png"),
     ];
     let mut router = Router::new();
     for (path, real_path) in static_routes {
-        let data: &'static str = String::leak(std::fs::read_to_string(
-            "frontend/static/".to_owned() + real_path,
-        )?);
-        router = router.route(path, get(move || async move { Html(data) }));
+        let data: &'static [u8] =
+            Vec::leak(std::fs::read("frontend/static/".to_owned() + real_path)?);
+        let content_type = match std::path::Path::new(real_path)
+            .extension()
+            .map(|x| x.to_str().unwrap())
+        {
+            Some("html") => "text/html; charset=utf-8",
+            Some("js") => "application/javascript",
+            Some("png") => "image/png",
+            _ => "",
+        };
+        router = router.route(
+            path,
+            get(move || async move { ([("content-type", content_type)], data) }),
+        );
     }
 
     router = router
         .route("/api/group/{group_id}/name", get(group_name))
         .route("/api/group/{group_id}/members", get(group_members))
         .route("/api/group/{group_id}/task", get(tasks))
-        .route("/api/group/{group_id}/task", post(create_task))
-        .route("/api/task/{task_id}/title", get(task_title));
-        // .route("/api/task/{task_id}", delete(delete_task));
+        .route("/api/task", post(create_task))
+        .route("/api/task/{task_id}/title", get(task_title))
+        .route("/api/group", post(create_group))
+        .route("/api/group/{group_id}/invite", post(invite_to_group))
+        .route("/api/group/{group_id}", delete(delete_group))
+        .route("/api/task/{task_id}", delete(delete_task));
+    // .route("/api/task/{task_id}", delete(delete_task));
 
     let listener = TcpListener::bind("0.0.0.0:3000").await?;
     println!("Starting at http://0.0.0.0:3000");
--- schema.sql
+++ schema.sql
@@ -21,9 +21,10 @@ create table if not exists tasks
 (
     id integer primary key not null,
     title text not null,
-    type text not null,
+    type text,
+    due integer,
     reward integer not null,
-    gr integer not null
+    gr integer
 );
 
 insert into groups values (0, 'inf339b');
@@ -31,3 +32,4 @@ insert into users values (0, 'jonas', 5);
 insert into users values (1, 'omfj', 6);
 insert into user_group values (0, 0);
 insert into user_group values (1, 0);
+insert into tasks values (0, 'do inf339b', null, null, 1000000, 0);
