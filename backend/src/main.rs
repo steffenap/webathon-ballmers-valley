e6b3478733c5a13af2ac112f00f7cfea793cb585
Jakob Skaar <jakobrs100@gmail.com>
2026-04-11 13:04:20 +0200

    abcde

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,19 +1,22 @@
 use axum::{Router, response::Html, routing::get};
 use tokio::net::TcpListener;
 
-async fn index() -> Html<&'static str> {
-    Html(include_str!("../../frontend/src/index.html"))
-}
-async fn login() -> Html<&'static str> {
-    Html(include_str!("../../frontend/src/login.html"))
-}
-
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    let router = Router::new()
-        .route("/", get(index))
-        .route("/login", get(login));
+    let static_routes = [
+        ("/", "index.html"),
+        ("/login", "login.html"),
+        ("/style.css", "style.css"),
+    ];
+    let mut router = Router::new();
+    for (path, real_path) in static_routes {
+        let data: &'static str = String::leak(std::fs::read_to_string(
+            "frontend/static/".to_owned() + real_path,
+        ).unwrap());
+        router = router.route(path, get(move || async move { Html(data) }));
+    }
 
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Starting at http://0.0.0.0:3000");
     axum::serve(listener, router).await.unwrap();
 }
